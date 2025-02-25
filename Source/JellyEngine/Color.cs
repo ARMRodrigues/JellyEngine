910bc9a6f39a7c010c2e0d87888ea5c90f17f717
Alvaro Rodrigues <alvaro.renan@yahoo.com.br>
2025-02-25 10:21:19 -0300

    feat: add support for Hex colors and helper functions for Color (addition, scalar division, lerp)

--- Source/JellyEngine/Color.cs
+++ Source/JellyEngine/Color.cs
@@ -15,20 +15,64 @@ public struct Color
 
     public Color(float red, float green, float blue)
     {
-       R = red;
-       G = green;
-       B = blue;
-       A = 1.0f;
+        R = red > 1.0 ? red / 255f : red;
+        G = green > 1.0 ? green / 255f : green;
+        B = blue > 1.0 ? blue / 255f : blue;
+        A = 1.0f;
     }
-    
+
     public Color(float red, float green, float blue, float alpha)
     {
-        R = red;
-        G = green;
-        B = blue;
+        R = red > 1.0 ? red / 255f : red;
+        G = green > 1.0 ? green / 255f : green;
+        B = blue > 1.0 ? blue / 255f : blue;
         A = alpha;
+    }
+
+    public Color(string hex)
+    {
+        // Remove the hash symbol if present
+        hex = hex.TrimStart('#');
+
+        if (hex.Length == 6) // RGB
+        {
+            R = Convert.ToInt32(hex.Substring(0, 2), 16) / 255f;
+            G = Convert.ToInt32(hex.Substring(2, 2), 16) / 255f;
+            B = Convert.ToInt32(hex.Substring(4, 2), 16) / 255f;
+            A = 1.0f;
+        }
+        else if (hex.Length == 8) // RGBA
+        {
+            R = Convert.ToInt32(hex.Substring(0, 2), 16) / 255f;
+            G = Convert.ToInt32(hex.Substring(2, 2), 16) / 255f;
+            B = Convert.ToInt32(hex.Substring(4, 2), 16) / 255f;
+            A = Convert.ToInt32(hex.Substring(6, 2), 16) / 255f;
+        }
+        else
+        {
+            throw new ArgumentException("Invalid hex color format. Use #RRGGBB or #RRGGBBAA.");
+        }
+    }
+
+    public static Color operator +(Color c1, Color c2)
+    {
+        return new Color(c1.R + c2.R, c1.G + c2.G, c1.B + c2.B, c1.A + c2.A);
+    }
+
+    public static Color operator /(Color c, float scalar)
+    {
+        return new Color(c.R / scalar, c.G / scalar, c.B / scalar, c.A / scalar);
     }
 
     public readonly Vector3 ToVector3() => new(R, G, B);
     public readonly Vector4 ToVector4() => new(R, G, B, A);
+    public static Color Lerp(Color a, Color b, float t)
+    {
+        return new Color(
+            a.R + (b.R - a.R) * t,
+            a.G + (b.G - a.G) * t,
+            a.B + (b.B - a.B) * t,
+            a.A + (b.A - a.A) * t
+        );
+    }
 }
