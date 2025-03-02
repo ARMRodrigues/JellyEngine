1a534cb8836b92a5ca7f2e0ece061665eb67e72f
Alvaro Rodrigues <alvaro.renan@yahoo.com.br>
2025-03-02 16:50:54 -0300

    feat: CanvasManagerSystem for JellyGame

--- Source/JellyGame/Scenes/Terrain/CanvasManagerSystem.cs
+++ Source/JellyGame/Scenes/Terrain/CanvasManagerSystem.cs
@@ -1,6 +1,20 @@
-﻿namespace JellyGame.Scenes.Terrain;
+﻿using JellyEngine;
+using JellyEngine.InputManagement;
 
-public class CanvasManagerSystem
+namespace JellyGame.Scenes.Terrain;
+
+public class CanvasManagerSystem (EntityManager entityManager): GameSystem
 {
+    private readonly EntityManager _entityManager = entityManager;
     
+    public override void Update()
+    {
+        foreach (var canvas in _entityManager.GetComponents<CanvasRenderer>())
+        {
+            if (Input.IsActionJustPressed("Map"))
+            {
+                canvas.IsVisible = !canvas.IsVisible;
+            }
+        }
+    }
 }
